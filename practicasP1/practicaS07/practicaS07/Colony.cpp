45395f1f378c69c433210ed605809648aec0fa27
Zirel-Rivera <zirel.rivera.hernandez@est.una.ac.cr>
2026-04-14 21:14:05 -0600

    colony.cpp

--- practicasP1/practicaS07/practicaS07/Colony.cpp
+++ practicasP1/practicaS07/practicaS07/Colony.cpp
@@ -1,14 +1,21 @@
 #include "Colony.h"
 
 namespace EIF201{
-	Colony::Colony(std::string _name, std::string planetLocation, int dome)
+	Colony::Colony(std::string _name, std::string planetLocation, int domeCount)
 	{
 		this->name = _name;
 		this->planetLocation = planetLocation;
-		this->dome = dome;
+		this->domeCount = 0;
+
 	}
 	Colony::~Colony()
 	{
+		for (int i = 0; i < domeCount; i++) {
+			delete dome[i];
+			dome[i] = nullptr;
+		}
+		delete[] dome;
+		dome = nullptr;
 	}
 
 	void Colony::setName(std::string newName) 
@@ -21,24 +28,45 @@ namespace EIF201{
 		this->planetLocation = location;
 	}
 
-	void Colony::setDome(int newDome) 
-	{
-		this->dome = dome;
-	}
-	
 	std::string Colony::getName() const { return name; }
 
 	std::string Colony::getPlanetLocation() const { return planetLocation; }
 
-	int Colony::getDome() const { return dome; }
+	Domes* Colony::getDome(int index) const{ 
+		if (index < 0 || index >= domeCount) {
+			return nullptr;
+		}
 
-	void Colony::createDome(int newDome)
-	{
-		   
+		return dome[index];
 	}
 
-	bool Colony::deleteDome(int newDome)
+	int Colony::getDomeCount() const{ return domeCount; }
+
+	void Colony::createDome(std::string typeDome, double capacityOxygen, int colonistShelter)
 	{
-		return false;
+		int newDomeId = domeCount + 1;
+		dome[domeCount] = new Domes[newDomeId, typeDome, capacityOxygen, colonistShelter];
+
+		domeCount++;
+
+		std::cout << "Se creo el domo " << newDomeId << " del tipo " << typeDome << std::endl;
+	}
+	
+
+	bool Colony::deleteDome(int index)
+	{
+		if (index < 0 || index >= domeCount) {
+			return false;
+		}
+
+		delete dome[index];
+
+		for (int i = index; i < domeCount - 1; i++) {
+			dome[i] = dome[i + 1];
+		}
+		dome[domeCount - 1] = nullptr;
+		domeCount--;
+
+		return true;
 	}
 }
--- practicasP1/practicaS07/practicaS07/Colony.h
+++ practicasP1/practicaS07/practicaS07/Colony.h
@@ -2,6 +2,8 @@
 #ifndef CONOLY_H
 #define COLONT_H
 #include <string>
+#include <iostream>
+#include "Domes.h"
 
 namespace EIF201 {
 
@@ -12,22 +14,23 @@ private:
 
 	std::string name;
 	std::string planetLocation;
-	int dome;
+	Domes** dome;
+	int domeCount;
 
 public:
-	Colony(std::string name, std::string planetLocation, int dome);
+	Colony(std::string name, std::string planetLocation, int domeCount);
 	~Colony();
 	
 	void setName(std::string newName);
 	void setPlanetLocation(std::string location);
-	void setDome(int newDome);
 
 	std::string getName() const;
 	std::string getPlanetLocation() const;
-	int getDome() const;
+	Domes* getDome(int index) const;
+	int getDomeCount() const;
 
-	void createDome(int newDome);
-	bool deleteDome(int newDome);
+	void createDome(std::string typeDome, double capacityOxygen, int colonistShelter);
+	bool deleteDome(int index);
 };
 
 }
