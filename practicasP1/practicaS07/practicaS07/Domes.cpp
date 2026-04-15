f09f794a6ee644aab90e8c8b958e0bddffdae1fc
Zirel-Rivera <zirel.rivera.hernandez@est.una.ac.cr>
2026-04-14 21:32:31 -0600

    create class Domes

--- practicasP1/practicaS07/practicaS07/Domes.cpp
+++ practicasP1/practicaS07/practicaS07/Domes.cpp
@@ -1,1 +1,41 @@
 #include "Domes.h"
+#include <iostream>
+
+namespace EIF201 {
+
+	Domes::Domes(int id, std::string typeDome, double capacityOxygen, int colonistShelter) 
+		: id(id), typeDome(typeDome), capacityOxygen(capacityOxygen), 
+		colonistShelter(colonistShelter) {}
+
+	Domes::~Domes()
+	{}
+
+	std::string Domes::getTypeDome() const {
+		return typeDome;
+	}
+	int Domes::getId() const {
+		return id;
+	}
+	int Domes::getColonistShelter() const{
+		return colonistShelter;
+	}
+	double Domes::getCapacityOxygen() const{
+		return capacityOxygen;
+	}
+
+	void setTypeDome(std::string typeDome) {
+		
+	}
+
+	void setId(int id) {
+
+	}
+
+	void setCapacityOxygen(double capacityOxygen) {
+
+	}
+
+	void emergencySealDome(int id) {
+
+	}
+}
--- practicasP1/practicaS07/practicaS07/Domes.h
+++ practicasP1/practicaS07/practicaS07/Domes.h
@@ -1,4 +1,32 @@
 #pragma once
-class Domes
-{};
+#ifndef DOMES.H
+#define DOMES.H
+#include <string>
 
+namespace EIF201 {
+	class Domes
+	{
+	private:
+
+		int id;
+		std::string typeDome;
+		double capacityOxygen;
+		int colonistShelter;
+
+	public:
+		Domes(int id, std::string typeDome, double capacityOxygen, int colonistShetter);
+		~Domes();
+
+		std::string getTypeDome() const;
+		int getId() const;
+		double getCapacityOxygen() const;
+		int getColonistShelter() const;
+
+		void setTypeDome(std::string typeDome);
+		void setId(int id);
+		void setCapacityOxygen(double capacityOxygen);
+
+		void emergencySealDome(int id);
+	};
+}
+#endif
